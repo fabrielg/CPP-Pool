5350eb4882219863b26c5047434f9ce407aa1fc4
Gabriel Francois <gfrancoi@f2r6s20.clusters.42paris.fr>
2025-10-25 17:20:34 +0200

    rework: use string iterator to upper char of strings

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
-#include <algorithm>
-using namespace std;
 
 #define DEFAULT_NOISE "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
+static void	display_upper_string(std::string message)
+{
+	for (std::string::iterator it = message.begin(); it != message.end(); it++)
+		std::cout << (char) std::toupper(*it);
+}
+
 int	main(int argc, char **argv)
 {
-	string	message;
+	std::string	message;
 
-	if (argc > 1)
+	if (argc > 1 && argv[1])
 	{
 		for (int i = 1; i < argc; i++)
 			message.append(argv[i]);
-		transform(message.begin(), message.end(), message.begin(), ::toupper);
-		cout << message;
+		display_upper_string(message);
 	}
 	else
-		cout << DEFAULT_NOISE;
-	cout << endl;
+		std::cout << DEFAULT_NOISE;
+	std::cout << std::endl;
 	return (0);
 }
