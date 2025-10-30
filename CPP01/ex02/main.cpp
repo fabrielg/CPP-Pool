2e0db3356c3f186aa8c1a2d3f759decb18a07a0c
Fabrielg <gfrancois2003@gmail.com>
2025-10-30 18:04:08 +0100

    feat: CPP01-ex02 add program

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int	main(void)
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << "Memory adress of string    : " << &string << std::endl;
+	std::cout << "Memory adress of stringPTR : " << &stringPTR << std::endl;
+	std::cout << "Memory adress of stringREF : " << &stringREF << std::endl;
+
+	std::cout << "Value of string    : " << string << std::endl;
+	std::cout << "Value of stringPTR : " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF : " << stringREF << std::endl;
+	return (1);
+}
