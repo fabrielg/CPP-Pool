0f4dfa3969c7489c79531bfc1d8dafcc27ec04fb
Fabrielg <gfrancois2003@gmail.com>
2025-12-13 15:41:36 +0100

    rework: CPP01-ex04 use ifstream and ofstream

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -2,43 +2,54 @@
 #include <fstream>
 #include <string>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	if (argc != 4)
-		return (1);
-
-	std::string		out_name = ((std::string) argv[1]).append(".replace");
-	std::ifstream	in(argv[1]);
-	std::ofstream	out(out_name.c_str());
-	
-	if (in.is_open() && out.is_open())
 	{
-		std::cout << "Success while opening file " << argv[1] << std::endl;
+		std::cerr << "Usage: ./replace <filename> <s1> <s2>" << std::endl;
+		return 1;
+	}
 
-		std::string	content((std::istreambuf_iterator<char>(in)),
-							(std::istreambuf_iterator<char>()));
+	std::string	filename = argv[1];
+	std::string	s1 = argv[2];
+	std::string	s2 = argv[3];
 
-		int	filter_size = ((std::string) argv[2]).size();
-		int	last_index = 0;
-		int	index = content.find(argv[2]);
-		while (index >= 0)
+	if (s1.empty())
+	{
+		std::cerr << "Error: s1 cannot be empty" << std::endl;
+		return 1;
+	}
+
+	std::ifstream	in(filename.c_str());
+	if (!in)
+	{
+		std::cerr << "Error: cannot open in file" << std::endl;
+		return 1;
+	}
+
+	std::ofstream	out((filename + ".replace").c_str());
+	if (!out)
+	{
+		std::cerr << "Error: cannot open out file" << std::endl;
+		return 1;
+	}
+
+	std::string	line;
+	while (std::getline(in, line))
+	{
+		size_t	pos = 0;
+
+		while ((pos = line.find(s1, pos)) != std::string::npos)
 		{
-			out << content.substr(last_index, index - last_index);
-			out << argv[3];
-			last_index = index + filter_size;
-			index = content.find(argv[2], last_index);
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+			pos += s2.length();
 		}
-		out << content.substr(last_index);
-		in.close();
-		out.close();
+
+		out << line;
+		if (!in.eof())
+			out << std::endl;
 	}
-	else
-	{
-		if (!in.is_open())
-			std::cout << "Error while opening file " << argv[1] << std::endl;
-		if (!out.is_open())
-			std::cout << "Error while opening file " << out_name << std::endl;
-		return (1);
-	}
-	return (0);
+
+	return 0;
 }
