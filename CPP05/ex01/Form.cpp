2129b589136242e26c8245b6fc221db1615dc85a
Fabrielg <Fabrielg>
2026-04-05 22:41:37 +0200

    feat: CPP05-ex01 add and setup canonical methods

--- CPP05/ex01/Form.cpp
+++ CPP05/ex01/Form.cpp
@@ -12,3 +12,18 @@ Form::~Form( void )
 {
 	std::cout << "Form " << _name << " has been destroyed" << std::endl;
 }
+
+
+Form::Form(const Form &copy):
+	_name(copy._name),
+	_signed(copy._signed),
+	_gradeCanSign(copy._gradeCanSign),
+	_gradeCanExec(copy._gradeCanExec)
+{
+	*this = copy;
+}
+
+Form	&Form::operator=( const Form &copy )
+{
+	return (*this);
+}
